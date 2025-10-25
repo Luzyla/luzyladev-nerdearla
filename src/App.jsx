bec92aaf37085c0596f44e9a640f6f46877c204a
Luzyla <luzyla@gmail.com>
2025-10-25 02:00:59 +0200

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from "react";
 import avatarLu from "./assets/luzyladev/LuzylaDev.png";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <picture>
-        <img src={avatarLu} alt="" />
-      </picture>
-      <h1>Nerdearla Luzyla Dev test</h1>
-      <div className="card"></div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="montserrat">Nerdearla Luzyla Dev test</h1>
+      <div className="card">
+        <div className="card__container-text">
+          <p className="card__text">Hey! I'm Lucila (a.k.a. Luzyla Dev)</p>
+          <button className="btn ">Add my contact just with one click!</button>
+        </div>
+
+        <picture className="card__container-image">
+          <img src={avatarLu} alt="" className="container-image__image" />
+        </picture>
+      </div>
     </>
   );
 }
